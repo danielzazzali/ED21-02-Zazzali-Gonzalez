1d2706ef76ac4edc865e605c964d90df78ed34ec
Daniel Zazzali <80864796+danielzazzali@users.noreply.github.com>
2021-10-22 21:07:21 -0300

    I

---
+++ FaceRecognition.cpp
@@ -0,0 +1,127 @@
+#include <opencv2/opencv.hpp>
+#include <vector>
+#include <string>
+#include <opencv2/opencv.hpp>
+#include <vector>
+#include <string>
+#include <fstream>
+#include "Identity.h"
+#include "LinkedList.h"
+
+// Components used
+using cv::Mat;
+using cv::waitKey;
+using cv::CascadeClassifier;
+using cv::COLOR_BGR2GRAY;
+using cv::Size;
+using cv::Rect;
+using cv::Scalar;
+using cv::Point;
+using cv::imread;
+using std::vector;
+using std::string;
+using std::cout;
+using std::endl;
+using std::stringstream;
+using cv::imwrite;
+using std::to_string;
+
+int main() {
+
+
+	// ---INITIALIZE COMPONENTS---
+	vector<Mat> images(7);
+	vector<string> labels(7);
+	string filename;
+	string name;
+	string filenumber;
+
+	// Scale for data processing
+	double scale = 1.5;
+
+	// Pre-trained machine learning model to detect faces
+	CascadeClassifier faceCascade;
+	faceCascade.load("C:/opencv/build/etc/haarcascades/haarcascade_frontalface_alt.xml");
+
+	// Open the image
+
+	vector<string> paths(7);
+	paths[0] = "Resources/personas1.jpg";
+	paths[1] = "Resources/personas2.jpg";
+	paths[2] = "Resources/personas3.jpg";
+	paths[3] = "Resources/personas4.jpg";
+	paths[4] = "Resources/personas5.jpg";
+	paths[5] = "Resources/personas6.jpg";
+	paths[6] = "Resources/personas7.jpg";
+
+	// ---PROCESS AND SHOW FRAMES---
+
+	for (int i = 0; i < paths.size(); i++) {
+
+		// Matrix to store the image
+		Mat frame = imread(paths[i]);
+
+		// Matrix to store the image on gray-scale color
+		Mat grayscale;
+
+		//Matrix to store the face cropped
+		Mat croppedFace;
+
+		// Convert the color of 'frame' to gray-scale and store it in 'grayscale'
+		cvtColor(frame, grayscale, COLOR_BGR2GRAY);
+		// Resize the image to make the process more efficient
+		resize(grayscale, grayscale, Size(grayscale.size().width / scale, grayscale.size().height / scale));
+
+		// Vector to store the area of the detected face
+		vector<Rect> faces;
+
+		// Detect and store the face in 'faces' as a list of rectangles
+		faceCascade.detectMultiScale(grayscale, faces, 1.02, 3);
+
+		for (int r = 0; r < faces.size(); r++) {
+
+			int x = faces[r].x * scale;
+			int y = faces[r].y * scale;
+			int w = faces[r].width * scale;
+			int h = faces[r].height * scale;
+
+			croppedFace = frame(Rect(x, y, w, h));
+
+			images[r] = croppedFace;
+
+			filenumber = to_string(r);
+			labels[r] = filenumber;
+
+			stringstream ssfn;
+			filename = "Resources\\Faces\\";
+			ssfn << filename.c_str() << name << filenumber << ".jpg";
+			filename = ssfn.str();
+			imwrite(filename, croppedFace);
+
+			//almacenar informacion de la identidad correspondiente
+
+		}
+
+		// For every rectangle in the face region
+		for (Rect area : faces) {
+
+			// Set the color to use (red)
+			Scalar drawColor = Scalar(0, 0, 255);
+
+			// Draws a rectangle around the area detected as a face
+			rectangle(frame,
+				Point(cvRound(area.x * scale), cvRound(area.y * scale)),
+				Point(cvRound((area.x + area.width - 1) * scale), cvRound((area.y + area.height - 1) * scale)),
+				drawColor, 2);
+
+		}
+
+		// Shows the frame in a windows
+		imshow("Frame", frame);
+		waitKey(1);
+
+	}
+
+	return 0;
+
+}
---
+++ LinkedList.cpp
@@ -0,0 +1,146 @@
+#include <iostream>
+#include "LinkedList.h"
+#include "Node.h"
+
+LinkedList::LinkedList() {
+
+	this->first = nullptr;
+	this->size = 0;
+
+}
+
+LinkedList::~LinkedList() {
+
+}
+
+Node* LinkedList::getFirst() {
+
+	return first;
+
+}
+
+Identity* LinkedList::getX(int x) {
+
+	if (x < 0 || x > size) {
+
+		return nullptr;
+
+	}
+
+	Node* p = first;
+
+	for (int i = 0; i < x; i++) {
+
+		p = p->getNext();
+
+	}
+
+	return p->getData();
+
+}
+
+int LinkedList::getSize() {
+
+	return size;
+
+}
+
+bool LinkedList::isEmpty() {
+
+	return size == 0;
+
+}
+
+bool LinkedList::add(Identity* identity) {
+
+	Node* node = new Node(identity);
+
+	if (size == 0) {
+
+		first = node;
+		first->setPrevious(nullptr);
+		first->setNext(nullptr);
+		size++;
+
+		return true;
+	}
+	else {
+
+		Node* p = first;
+
+		while (p->getNext() != nullptr) {
+
+			p = p->getNext();
+
+		}
+
+		node->setPrevious(p);
+		p->setNext(node);
+		node->setNext(nullptr);
+		size++;
+
+		return true;
+
+	}
+
+	return false;
+
+}
+
+
+bool LinkedList::remove(int x) {
+
+	if (x < 0 || x >= size || size == 0) {
+
+		return false;
+
+	}
+
+	if (x == 0) {
+
+		if (size == 1) {
+
+			first = nullptr;
+			size--;
+			return true;
+
+		}
+
+		first = first->getNext();
+		first->setPrevious(nullptr);
+		size--;
+
+		return true;
+	}
+
+	Node* p = first;
+	
+	if (x > 0 && x < size - 1) {
+
+		for (int i = 0; i < x; i++) {
+
+			p = p->getNext();
+
+		}
+
+		p->getNext()->setPrevious(p->getPrevious());
+		p->getPrevious()->setNext(p->getNext());
+		size--;
+
+		return true;
+
+	}
+
+	while (p->getNext() != nullptr) {
+
+		p = p->getNext();
+
+	}
+
+	p->getPrevious()->setNext(nullptr);
+	delete p;
+	size--;
+
+	return true;
+	
+}

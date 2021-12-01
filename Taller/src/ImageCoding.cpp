c8b5175fa33b28839ef61bbd2d7c09cbfdb81702
Daniel Zazzali <80864796+danielzazzali@users.noreply.github.com>
2021-11-30 21:24:36 -0300

    Actualizado ImageCoding.cpp

--- Taller/src/ImageCoding.cpp
+++ Taller/src/ImageCoding.cpp
@@ -15,17 +15,22 @@ std::vector<Mat> ImageCoding::codeGray(std::vector<Rect> detections, bool equali
 
     std::vector<Mat> cropFaces;
     for (const auto& det : detections) {
+
         // Recorto la imagen detectada
         cropColor = frame_(det);
+
         //Usamos solo imágenes en grises
         cvtColor(cropColor, imageGray, COLOR_BGR2GRAY);
+
         // Si se desea ecualizar la imagen (aumentar contraste)
         if (equalize) {
             equalizeHist(imageGray, imageGray);
         }
         equalizeHist(imageGray, imageGray);
+
         // Reescalamos
         resize(imageGray, resizedDown, size, INTER_LINEAR);
+
         cropFaces.push_back(resizedDown.clone());
     }
 

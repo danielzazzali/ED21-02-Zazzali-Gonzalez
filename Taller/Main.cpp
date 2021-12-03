83f78018a90b3bf6a9e65e42f01182e877302085
Diego González <90357250+DiegoGonzalez07@users.noreply.github.com>
2021-12-03 17:31:04 -0300

    Update: Seleccion inicio y final del video

--- Taller/Main.cpp
+++ Taller/Main.cpp
@@ -3,6 +3,7 @@
 #include "opencv2/imgproc.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/core.hpp"
+#include <opencv.hpp>
 
 #include "src/include/FaceDetector.h"
 #include "src/include/ImageCoding.h"
@@ -21,16 +22,38 @@ int main() {
     string path = "resources/Personas.mp4";
     VideoCapture cap(path);
 
-    while (true)
+    int segundos = cap.get(CAP_PROP_FRAME_COUNT)/ cap.get(CAP_PROP_FPS); //Calculo la cantidad de segundos del video
+    double comienzo,final; 
+
+    std::cout << "El video dura " << segundos << " segundos" << std::endl;
+
+    //Inicio del video
+    std::cout << "¿Desde que segundo quiere comenzar?" << std::endl;
+    std::cin >> comienzo;
+    while (comienzo < 0 || comienzo > segundos)
+    {
+        std::cout << "Segundos fuera de rango, intente de nuevo" << std::endl;
+        std::cin >> comienzo;
+    }
+    int comienzoMilisegundos = comienzo * 1000; //Paso segundos a milisegundos para setearlos en el video
+    cap.set(CAP_PROP_POS_MSEC, comienzoMilisegundos);
+
+    //Final del video
+    std::cout << "¿Hasta que segundo?" << std::endl;
+    std::cin >> final;
+    while (final < comienzo || final > segundos)
+    {
+        std::cout << "Segundos fuera de rango, intente de nuevo" << std::endl;
+        std::cin >> final;
+    }
+    int finalFrames = final * cap.get(CAP_PROP_FPS); //Paso los segundos a fotogramas
+
+    //Mientras que el fotograma en el que se encuentra sea menor o igual al fotograma final, entonces que detecte caras
+    while (cap.get(CAP_PROP_POS_FRAMES) <= finalFrames)
     {
         Mat image;
         cap.read(image);
-
-        if (image.empty())
-        {
-            break;
-        }
-
+ 
         FaceDetector fdetector;
         ImageCoding icoding;
 
@@ -80,23 +103,24 @@ int main() {
 
         // Mostrar la imagen con las marcas (rectángulos) indicando la posición de la cara
         imshow("Detected Face", image);
-        waitKey(1);
+        cv::waitKey(1);
+
     }
     //Vector que guardara los nodos del arbol binario
     vector<BinaryNode> arreglo;
 
     tree->print();
-    cout << "La altura es: " << tree->height() << endl;
+    std::cout << "La altura es: " << tree->height() << endl;
 
     int* max = tree->findMax();
-    cout << "Id con mayor tiempo en pantalla:  ID = " << max[1] << " Frames = " << max[0] << "\n" << endl;
+    std::cout << "Id con mayor tiempo en pantalla:  ID = " << max[1] << " Frames = " << max[0] << "\n" << endl;
 
     tree->show(1,arreglo); //Se llena el vector con los nodos
-    sort(arreglo.begin(), arreglo.end()); // se ordena de mayor a menor
+    std::sort(arreglo.begin(), arreglo.end()); // se ordena de mayor a menor
 
-    cout << "Las 5 caras que mas se repitieron son:" << endl;
+    std::cout << "Las 5 caras que mas se repitieron son:" << endl;
     for (int i = 0; i < 5; i++) {
-        cout << "ID: " << arreglo[i].getIdentifier() << " Frames: " << arreglo[i].getFrames()<<" " << "Segundos: " << float (arreglo[i].getFrames() / 10.) << endl;
+        std::cout << "ID: " << arreglo[i].getIdentifier() << " Frames: " << arreglo[i].getFrames()<<" " << "Segundos: " << float (arreglo[i].getFrames() / cap.get(CAP_PROP_FPS)) << endl;
     }
 
 
@@ -107,7 +131,7 @@ int main() {
         int ident = list->getX(i)->getidentifier();
         putText(face,to_string(ident), Point(20, 20), 1, 1.5, Scalar(0, 0, 0), 2, 8, false);
         imshow(to_string(ident), face);
-        waitKey(0);
+        cv::waitKey(0);
     }
 
     destroyAllWindows();
